72e9efaa8efe5dd510f86719601ef713f2924884
Gilbert Haro <gilbertharocode@gmail.com>
2023-03-18 23:09:34 -0700

    graphiql

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,13 +8,27 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  siteMetadata: {
+    title: 'Simply Recipes',
+    description: 'Nice and clean recipes site',
+    author: '@gilbertharo',
+    person: { name: 'gilbert', age: 28 },
+    simpleData: ['item 1', 'item 2'],
+    complexData: [
+      { name: 'gilbert', age: 28 },
+      { name: 'steph', age: 28 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`, // Needed for dynamic images
+    `gatsby-plugin-styled-components`,
     {
-      resolve: `gatsby-plugin-styled-components`,
+      resolve: `gatsby-source-filesystem`,
       options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
         // Add any options here
       },
     },
